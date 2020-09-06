ef62be9b12df3a9710b7992d4b08837d66b75fd5
João Domingues <jpdomingues@criticalsoftware.com>
2020-09-06 13:55:16 +0100

    Play template implemented

---
+++ web/src/main/java/com/jpdomingues/util/AttributeNames.java
@@ -0,0 +1,11 @@
+package com.jpdomingues.util;
+
+public final class AttributeNames {
+
+    // == cosntants == //
+    public static final String MAIN_MESSAGE = "mainMessage";
+    public static final String RESULT_MESSAGE = "resultMessage";
+
+    // == Constructor == //
+    private AttributeNames(){}
+}
--- web/src/main/java/com/jpdomingues/util/ViewNames.java
+++ web/src/main/java/com/jpdomingues/util/ViewNames.java
@@ -4,6 +4,9 @@ public class ViewNames {
 
     //== fields ==//
     public static final String HOME = "home";
+    public static final String PLAY = "play";
+    public static final String GAME_OVER = "game-over";
+
 
     //== constructors ==//
     private ViewNames(){}
