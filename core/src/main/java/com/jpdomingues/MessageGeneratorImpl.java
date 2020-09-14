3c5029eca1cb002cd0cd75439ae1339a85949572
João Domingues <jpdomingues@criticalsoftware.com>
2020-09-14 20:53:22 +0100

    i18n implemented; Interceptors added

--- core/src/main/java/com/jpdomingues/MessageGeneratorImpl.java
+++ core/src/main/java/com/jpdomingues/MessageGeneratorImpl.java
@@ -2,6 +2,8 @@ package com.jpdomingues;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -10,11 +12,24 @@ import javax.annotation.PostConstruct;
 @Component
 public class MessageGeneratorImpl implements MessageGenerator {
 
+    //== Constants ==//
+    private static final String MAIN_MESSAGE = "game.main.message";
+    private static final String WIN = "game.win";
+    private static final String LOSE = "game.lose";
+    private static final String INVALID_RANGE = "game.invalid.range";
+    private static final String FIRST_GUESS = "game.first.guess";
+    private static final String HIGHER = "game.higher";
+    private static final String LOWER = "game.lower";
+    private static final String REMAINING = "game.remaining";
+
    // == fields ==//
    private final Game game;
 
+   private final MessageSource messageSource;
+
    //== constructors ==//
-    public MessageGeneratorImpl(Game game) {
+    public MessageGeneratorImpl(Game game, MessageSource messageSource) {
+        this.messageSource = messageSource;
         this.game = game;
     }
 
@@ -26,31 +41,37 @@ public class MessageGeneratorImpl implements MessageGenerator {
 
     @Override
     public String getMainMessage() {
-        return "Number is between " + game.getSmallest() + " and " +
-                                    + game.getBiggest() + ". " +
-                "Can you guess it?";
+        return getMessage(MAIN_MESSAGE, game.getSmallest(), game.getBiggest());
+
+//        return "Number is between " + game.getSmallest() + " and " +
+//                                    + game.getBiggest() + ". " +
+//                "Can you guess it?";
     }
 
     @Override
     public String getResultMessage() {
 
         if(game.isGameWon()){
-            return "You guessed it! The number was " + game.getNumber();
+            return this.getMessage(WIN, game.getNumber());
         } else if(game.isGameLost()) {
-            return "You lost. The number was " + game.getNumber();
+            return this.getMessage(LOSE, game.getNumber());
         } else if (!game.isValidNumberRange()){
-            return "Invalid number range!";
+            return this.getMessage(INVALID_RANGE);
         } else if (game.getRemainingGuesses() == game.getGuessCount()){
-            return  "What is your first guess?";
+            return this.getMessage(FIRST_GUESS);
         } else {
-            String direction = "Lower";
+            String direction = getMessage(LOWER);
 
             if(game.getGuess() < game.getNumber()){
-                direction = "Higher";
+                direction = getMessage(HIGHER);
             }
 
-            return direction + "! You have " + game.getRemainingGuesses() +
-                    " guesses left";
+            return getMessage(REMAINING, direction, game.getRemainingGuesses());
         }
+    }
+
+    // == private methods == //
+    private String getMessage(String code, Object... args){
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 }
--- web/src/main/java/com/jpdomingues/config/WebConfig.java
+++ web/src/main/java/com/jpdomingues/config/WebConfig.java
@@ -1,16 +1,37 @@
 package com.jpdomingues.config;
 
+import com.jpdomingues.interceptor.RequestInterceptor;
 import com.jpdomingues.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    // == bean methods ==//
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new SessionLocaleResolver();
+    }
+
+    // == configurations == //
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/").setViewName(ViewNames.HOME);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
+        registry.addInterceptor(new LocaleChangeInterceptor());
+    }
+
 
 }
